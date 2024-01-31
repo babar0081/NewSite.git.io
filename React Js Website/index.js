bc92b1899061c682b985f139d6e3e2f91f875c4a
babar0081 <104710048+babar0081@users.noreply.github.com>
2024-01-31 12:52:09 +0500

    1

--- React Js Website/index.js
+++ React Js Website/index.js
@@ -3,6 +3,12 @@ const app = express()
 const dotenv = require("dotenv").config();
 const PORT = process.env.Port || 4000;
 
+
+app.use("/" ,(req ,res )=>{
+
+    res.send("hello world my website is running :D")
+})
+
 app.listen(PORT, ()=>{
     console.log(`our server is running perfectlly ${PORT}..`)
 })
