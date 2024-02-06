40f1c23b4ec5bcc16a17f2f81744a687408b10ea
babar0081 <104710048+babar0081@users.noreply.github.com>
2024-02-06 16:35:11 +0500

    1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,13 @@
 import Navbar from "../features/Navbar/Navbar";
 import ProductList from "../features/Product-list/ProductList";
-import LoginPage from "./LoginPage";
 
 function Home() {
     return ( 
         <div>
-            {/* <Navbar>
+            <Navbar>
                 <ProductList></ProductList>
-            </Navbar> */}
-            <LoginPage></LoginPage>
+            </Navbar>
+            
 
         </div>
 
