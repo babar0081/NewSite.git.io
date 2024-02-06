a2df4fea23c84f93eaf7100b0c9fc58fd3444201
babar0081 <104710048+babar0081@users.noreply.github.com>
2024-02-06 11:45:10 +0500

    154

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Productlist from './features/Product-list/ProductList.js'
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 function App() {
   return (
     <div className='App'>
       <div>
           <Productlist></Productlist>
+          <SpeedInsights />
         </div>
     </div>
   );
