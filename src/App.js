444725ada7e0ff429d476aa189e8b2d0e46e4925
babar0081 <104710048+babar0081@users.noreply.github.com>
2024-02-06 11:58:23 +0500

    5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Productlist from './features/Product-list/ProductList.js'
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
 
 
